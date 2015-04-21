23ddf4e9cfb4dc3c4e7b3c842eb9b57aec2ce6bf
Keen <kyliau@umich.edu>
2015-04-20 23:52:16 -0400

    first implementation

--- utah-trip/processpayment.py
+++ utah-trip/processpayment.py
@@ -30,7 +30,7 @@ class Expense(ndb.Model):
     details = ndb.StringProperty(indexed=False)
     amount = ndb.FloatProperty(indexed=False)
     paidBy = ndb.StringProperty(indexed=True)
-    split = ndb.FloatProperty(repeated=True)
+    individualAmount = ndb.FloatProperty(repeated=True)
 
 class Person(ndb.Model):
     """ A main model for representing a user """
@@ -65,62 +65,76 @@ class MainPage(webapp2.RequestHandler):
 
 class ExpenseTracker(webapp2.RequestHandler):
     def post(self):
-        print self.request
-    
         persons_query = Person.query(
-            ancestor=person_key(DEFAULT_EXPENSE)).order(Person.name)
+            ancestor=person_key(DEFAULT_PERSON)).order(Person.name)
         persons = persons_query.fetch()
-
-        split = []
-        for person in persons:
-            elemId = persons.shortName + 'Amount'
-            split.append(float(self.request.get(elemId, 0)))
 
         expense = Expense(parent=expense_key(DEFAULT_EXPENSE))
         expense.transactionDate = datetime.datetime.strptime(self.request.get('date'), "%Y-%m-%d")
         expense.details = self.request.get('details')
         expense.amount = float(self.request.get('amount', 0))
         expense.paidBy = self.request.get('paidBy')
-        expense.split = split
-
-        print expense
-
+        for person in persons:
+            elemId = person.shortName + 'Amount'
+            individualAmount = self.request.get(elemId, 0) or 0
+            expense.individualAmount.append(float(individualAmount))
         expense.put()
+
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write("Successfully added expense!")
-        #self.redirect('/admin')
+        self.redirect('/admin?user=%s' % expense.paidBy)
 
 class Admin(webapp2.RequestHandler):
     def get(self):
+        user = self.request.get('user')
+        person_query = Person.query(Person.name == user)
+        person = person_query.fetch()
+
+        if not person:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('%s is not found' % user)
+            return
+
+        user = person[0]
         expenses_query = Expense.query(
             ancestor=expense_key(DEFAULT_EXPENSE)).order(-Expense.transactionDate, -Expense.createdDate)
         expenses = expenses_query.fetch()
 
-        paidByKai = 0
-        sharedByKai = 0
+        totalPaid = 0
+        totalSpent = 0
+        userExpenses = []
         for expense in expenses:
-            if (expense.paidBy == 'Kai'):
-                paidByKai += expense.amount
-            sharedByKai += expense.kaiAmount
+            addToArray = False
+            if expense.paidBy == user.name:
+                totalPaid += expense.amount
+                addToArray = True
+            if expense.individualAmount[user.userId] > 0:
+                totalSpent += expense.individualAmount[user.userId]
+                addToArray = True
+            if addToArray:
+                userExpenses.append(expense)
 
-        oweToKai = paidByKai - sharedByKai
+        amountOwed = totalSpent - totalPaid
         
         message = ''
         alertType = 'alert-info'
-        if abs(oweToKai) < 0.05:
+        if abs(amountOwed) < 0.05:
             message = 'All dues are clear'
             alertType = 'alert-success'
-        elif oweToKai < 0:
-            message = 'Kai owes Keen USD ' + "{:10.2f}".format(abs(oweToKai))
+        elif amountOwed > 0:
+            message = '%s owes Auntie Terry USD %s' % (user.name, "{:10.2f}".format(amountOwed))
         else:
-            message = 'Keen owes Kai USD ' + "{:10.2f}".format(oweToKai)
+            message = 'Auntie Terry owes %s USD %s' % (user.name, "{:10.2f}".format(abs(amountOwed)))
         print message
 
         template = jinja_environment.get_template('admin.html')
         self.response.out.write(template.render({
-            'expenses':expenses,
-            'alertType':alertType,
-            'message':message
+            'user': user,
+            'expenses': userExpenses,
+            'alertType': alertType,
+            'message': message,
+            'totalPaid': totalPaid,
+            'totalSpent': totalSpent
         }))
 
 app = webapp2.WSGIApplication([
