db65e3cfd2382e4d3a6f68e7b7445b09c0243e7f
Keen <kyliau@umich.edu>
2016-06-23 23:15:24 -0400

    update

--- expense--tracker/src/handlers/home.py
+++ expense--tracker/src/handlers/home.py
@@ -2,7 +2,7 @@ from google.appengine.api import users
 import os
 import jinja2
 import webapp2
-import ettypes
+#import ettypes
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -11,19 +11,20 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        print "something happened"
-        user = users.get_current_user()
-        if user:
-            appUser = ettypes.AppUser.queryByUserId(user.user_id())
-            if appUser:
-                self.redirect('/home')
-            else:
-                self.redirect('/register')
-        else:
-            url = users.create_login_url(self.request.uri)
-            template_values = { 'url': url }
-            template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-            self.response.write(template.render(template_values))
+        self.response.write("Hello World")
+        #print "something happened"
+        #user = users.get_current_user()
+        #if user:
+        #    appUser = ettypes.AppUser.queryByUserId(user.user_id())
+        #    if appUser:
+        #        self.redirect('/home')
+        #    else:
+        #        self.redirect('/register')
+        #else:
+        #    url = users.create_login_url(self.request.uri)
+        #    template_values = { 'url': url }
+        #    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        #    self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
