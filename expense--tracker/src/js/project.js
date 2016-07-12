0ad776d56f83c9535d1d5d55f50bae4fd2c6f22c
Keen <kyliau@umich.edu>
2016-07-11 21:12:30 -0400

    update

--- expense--tracker/src/js/project.js
+++ expense--tracker/src/js/project.js
@@ -4,96 +4,90 @@ function getTodayDate() {
     var mm = today.getMonth() + 1;
     var yyyy = today.getFullYear();
     if (dd < 10) {
-        dd = '0' + dd;
+        dd = "0" + dd;
     }
     if (mm < 10) {
-        mm = '0' + mm;
+        mm = "0" + mm;
     }
-    return yyyy + '-' + mm + '-' + dd;
+    return yyyy + "-" + mm + "-" + dd;
 }
 
-function foo() {
-  var list = $('.participant').filter(function() {
-    return this.checked;
-  }).map(function() {
-    return this.value;
-  }).get();
-  return list;
-};
-
 function calculateSplit() {
-    var amountInput = $('input[name=amount]');
+    var amountInput = $("input[name=amount]");
     var total = parseFloat(amountInput.val());
-    var splits = $('.participant:checked');
+    var splits = $(".participant:checked");
     var count = splits.length;
     splits.each(function() {
-        $('input[name=' + this.value + ']').val(total / count);
+        $("input[name=" + this.value + "]").val(total / count);
     });
 }
 
 $(document).ready(function() {
-    var NUM_PARTICIPANT = $('option').length;
+    var NUM_PARTICIPANT = $("option").length;
 
-    $('input[name=date]').val(getTodayDate());
+    $("input[name=date]").val(getTodayDate());
 
-    $('.participant').change(function() {
-        var inputElem = $('input[name=' + this.value + ']');
+    $(".participant").change(function() {
+        var inputElem = $("input[name=" + this.value + "]");
         if (this.checked) {
-            inputElem.closest('.form-group').show();
+            inputElem.closest(".form-group").show();
         } else {
-            inputElem.closest('.form-group').hide();
+            inputElem.closest(".form-group").hide();
         }
-        var flag = $('.participant:checked').length === NUM_PARTICIPANT;
-        $('#splitAll').prop('checked', flag);
+        var flag = $(".participant:checked").length === NUM_PARTICIPANT;
+        $("#splitAll").prop("checked", flag);
     });
 
-    $('#splitAll').click(function() {
-        $('.participant').prop('checked', this.checked)
-                         .trigger('change');
+    $("#splitAll").click(function() {
+        $(".participant").prop("checked", this.checked)
+                         .trigger("change");
     });
 
-    var previousPerson = '';
-    $('select[name=paid_by]').click(function() {
+    var previousPerson = "";
+    $("select[name=paid_by]").click(function() {
         previousPerson = this.value;
     }).change(function() {
-        if (!$('#splitAll').is(':checked')) {
+        if (!$("#splitAll").is(":checked")) {
             var currentPerson = this.value;
-            $('#' + previousPerson).prop('checked', false)
-                                   .trigger('change');
-            $('#' + currentPerson).prop('checked', true).trigger('change');
+            $("#" + previousPerson).prop("checked", false)
+                                   .trigger("change");
+            $("#" + currentPerson).prop("checked", true).trigger("change");
         }
-    }).trigger('change');
+    }).trigger("change");
 
     $("#splitEqually").click(function() {
         if (this.checked) {
-            $('#individualAmount').hide();
+            $("#individualAmount").hide();
         } else {
-            $('#individualAmount input').val('');
-            $('#individualAmount').show();
+            $("#individualAmount input").val("");
+            $("#individualAmount").show();
         }
     });
 });
 
 $(document).ready(function() {
-    $('form').submit(function(event) {
-        $('input[name=split_with]').val(foo().join(','));
-        var details = $('input[name=details]').val().trim();
+    $("form").submit(function(event) {
+        var splitWith = $(".participant").filter(() => this.checked)
+                                         .map(() => this.value)
+                                         .get();
+        $("input[name=split_with]").val(splitWith.join(","));
+        var details = $("input[name=details]").val().trim();
         if (details.length === 0) {
             alert("Please enter transaction details");
             event.preventDefault();
             return;
         }
 
-        var amountInput = $('input[name=amount]');
+        var amountInput = $("input[name=amount]");
         var total = parseFloat(amountInput.val());
         if (isNaN(total) || total <= 0) {
             alert("Amount is invalid");
             event.preventDefault();
             return;
         }
-            
-        if ($("#splitEqually").is(':checked')) {
-            if ($('.participant:checked').length === 0) {
+
+        if ($("#splitEqually").is(":checked")) {
+            if ($(".participant:checked").length === 0) {
                 alert("Must choose at least one person to split");
                 event.preventDefault();
                 return;
@@ -101,7 +95,7 @@ $(document).ready(function() {
             calculateSplit();
         } else {
             var sum = 0;
-            $('#individualAmount input').each(function() {
+            $("#individualAmount input").each(function() {
                 var amount = parseFloat(this.value);
                 if (!isNaN(amount)) {
                     sum += amount;
