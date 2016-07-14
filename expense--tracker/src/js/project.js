475b1355e7c6cf5c7bbf9c3f13c570a4d5c4cb2f
Keen <kyliau@umich.edu>
2016-07-13 22:33:22 -0400

    fix all eslint warnings and errors

--- expense--tracker/src/js/project.js
+++ expense--tracker/src/js/project.js
@@ -1,9 +1,9 @@
 function getTodayDate() {
     // Return today's date in yyyy-mm-dd format.
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1;
-    let yyyy = today.getFullYear();
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
     if (dd < 10) {
         dd = "0" + dd;
     }
@@ -16,28 +16,28 @@ function getTodayDate() {
 function calculateSplit() {
     // Write the amount of each individual to the input element when
     // amount is split equally.
-    let amountInput = $("input[name=amount]");
-    let total = parseFloat(amountInput.val());
-    let count = $(".participant:checked").length;
+    var amountInput = $("input[name=amount]");
+    var total = parseFloat(amountInput.val());
+    var count = $(".participant:checked").length;
     $(".participant").each(function() {
-        let amount = this.checked ? total / count : 0;
+        var amount = this.checked ? total / count : 0;
         $("input[name=" + this.value + "]").val(amount);
     });
 }
 
 $(document).ready(function() {
-    let numParticipants = $("option").length;
+    var numParticipants = $("option").length;
 
     $("input[name=date]").val(getTodayDate());
 
     $(".participant").change(function() {
-        let inputElem = $("input[name=" + this.value + "]");
+        var inputElem = $("input[name=" + this.value + "]");
         if (this.checked) {
             inputElem.closest(".form-group").show();
         } else {
             inputElem.closest(".form-group").hide();
         }
-        let flag = $(".participant:checked").length === numParticipants;
+        var flag = $(".participant:checked").length === numParticipants;
         $("#splitAll").prop("checked", flag);
     });
 
@@ -46,12 +46,12 @@ $(document).ready(function() {
                          .trigger("change");
     });
 
-    let previousPerson = "";
+    var previousPerson = "";
     $("select[name=paid_by]").click(function() {
         previousPerson = this.value;
     }).change(function() {
         if (!$("#splitAll").is(":checked")) {
-            let currentPerson = this.value;
+            var currentPerson = this.value;
             $("#" + previousPerson).prop("checked", false)
                                    .trigger("change");
             $("#" + currentPerson).prop("checked", true).trigger("change");
@@ -70,19 +70,24 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $("form").submit(function(event) {
-        let splitWith = $(".participant").filter(() => this.checked)
-                                         .map(() => this.value)
-                                         .get();
+        var splitWith = $(".participant")
+        .filter(function() {
+            return this.checked;
+        })
+        .map(function() {
+            return this.value;
+        })
+        .get();
         $("input[name=split_with]").val(splitWith.join(","));
-        let details = $("input[name=details]").val().trim();
+        var details = $("input[name=details]").val().trim();
         if (details.length === 0) {
             alert("Please enter transaction details");
             event.preventDefault();
             return;
         }
 
-        let amountInput = $("input[name=amount]");
-        let total = parseFloat(amountInput.val());
+        var amountInput = $("input[name=amount]");
+        var total = parseFloat(amountInput.val());
         if (isNaN(total) || total <= 0) {
             alert("Amount is invalid");
             event.preventDefault();
@@ -97,9 +102,9 @@ $(document).ready(function() {
             }
             calculateSplit();
         } else {
-            let sum = 0;
+            var sum = 0;
             $("#individualAmount input").each(function() {
-                let amount = parseFloat(this.value);
+                var amount = parseFloat(this.value);
                 if (!isNaN(amount)) {
                     sum += amount;
                 }
